a75baeec99a3f274658876564fdbe87b6df415d8
Stiven Diaz <stivendiazagudelo@gmail.com>
2018-12-09 16:45:06 -0500

    cambiamos logica

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res){
-    if(req.query.nombre){
-    res.send('<h1>Hola '+req.query.nombre+'!</h1>');
-    }else{res.send('<h1>Hola desconocido!</h1>');}
+app.get('/makers/:name', function(req, res){
+    if(req.params){
+    res.send('<h1>Hola '+req.params.name.charAt(0).toUpperCase() + req.params.name.slice(1)+'!</h1>');
+    }else{res.send('<h1>hola desconocido!</h1>');}
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
