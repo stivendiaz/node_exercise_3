180f97447fa162b9087bd458b8a40167d9e4f90f
Stiven Diaz <stivendiazagudelo@gmail.com>
2018-12-09 16:23:58 -0500

    arreglamos mensaje

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
+    if(req.query.nombre){
     res.send('<h1>hola '+req.query.nombre+'!</h1>');
+    }else{res.send('<h1>hola desconocido!</h1>');}
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
